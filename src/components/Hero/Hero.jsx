d4fc85842da945eb8ed6684c426aed63652b17cb
Arpit Sharma <arpitsharma7736@gmail.com>
2024-02-07 15:03:52 +0530

    setup gh-pages

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Arpit</h1>
+        <p className={styles.description}>
+          I'm a frontend developer with 2 years of experience using React and
+          NodeJS-environment. Reach out if you'd like to know more!
+        </p>
+        <a href="mailto:arpitsharma7736@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
